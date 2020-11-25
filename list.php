6560c962ee69ea22a8148a2f1b60cbc30c3a9ba1
Klas Mannberg <klas.mannberg@gmail.com>
2020-11-25 12:52:12 +0100

    Produktlista

--- list.php
+++ list.php
@@ -1,37 +1,56 @@
 <html>
+
 <body>
-<head>
-    <link rel="stylesheet" href="css/products.css" />
-</head>
-<p class=serif> List of products </p>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "123";
-$dbname = "myDB";
+  <p>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+    <head>
+      <link rel="stylesheet" href="css/products.css" />
+    </head>
+    <h1 class=serif> Produktlista: </h1>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "123";
+    $dbname = "myDB";
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-$sql = "SELECT * FROM produkt";
-$result = $conn->query($sql);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["ProductID"]. " - Name: " . $row["ProductName"]. " 
-    - Pris: " . $row["Pris"]. " - Saldo: " . $row["Lagersaldo"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM produkt";
+    $result = $conn->query($sql);
 
-?>
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<span id=\"lst\"><strong>ProductID: </strong>" . $row["ProductID"]
+          . " | <strong>Produkt Namn: </strong>" . $row["ProductName"]
+          . " |<strong> Datum: </strong>" . $row["Tillagt datum"]
+          . " | <strong>Pris: </strong>" . $row["Pris"]
+          . " | <strong>Lagersaldo: </strong>" . $row["Lagersaldo"]
+          . "</span><br>";
+      }
+    } else {
+      echo "Inga resultat!";
+    }
+    $sql = "SELECT * FROM kategorier";
+    $result = $conn->query($sql);
+    echo "<br><h1 class=serif>Kategorilista: </h1>";
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<span id=\"lst\"><strong>ProductID FK: </strong>" . $row["ProductID"]
+          . " | <strong>Kategori Namn: </strong>" . $row["ProductName"]
+          . "</span><br>";
+      }
+    } else {
+      echo "Inga resultat!";
+    }
+    $conn->close();
 
+    ?>
+    </div>
 </body>
+
 </html>
